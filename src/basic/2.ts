2e2d4090c6704e2473138bf940120263bd183f82
PushEax <pusheax.vk@gmail.com>
2025-05-14 17:20:00 +0300

    complete basic 2

--- src/basic/2.ts
+++ src/basic/2.ts
@@ -4,4 +4,6 @@
 // let person = ['Max', 21];
 // Як переписати його в TypeScript, використовуючи концепцію кортежів, щоб гарантувати, що перший елемент завжди буде рядком, а другий числом?
 
-let person = ['Max', 21];
+type Person = [string, number];
+
+let person: Person = ["Max", 21];
