4800ef6eacdbdb3df4c1f774a5ed0cbc7afa0124
PushEax <pusheax.vk@gmail.com>
2025-05-14 17:47:16 +0300

    complete basic 3

--- src/basic/3.ts
+++ src/basic/3.ts
@@ -1,3 +1,9 @@
 // Завдання 3
 // Виконуйте це завдання у файлі src/basic/3.ts.
 // Створіть змінну, яка може містити або рядок, або число(union type) ? Також, оголосіть змінну, яка може містити лише одне з двох можливих рядкових значень: 'enable' або 'disable'(literal type).
+
+type StringOrNumber = string | number;
+type EnableOrDisable = "enable" | "disable";
+
+const wordOrNumber: StringOrNumber = "Hello";
+const isEnable: EnableOrDisable = "disable";
