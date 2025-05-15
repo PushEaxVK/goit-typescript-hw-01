59526975b0dedca68c119c5a7f66fd52d2719373
PushEax <pusheax.vk@gmail.com>
2025-05-15 14:05:25 +0300

    complete generics 4

--- src/generics/4.ts
+++ src/generics/4.ts
@@ -21,3 +21,20 @@
 //   email: 'user@mail.com',
 //   password: 'password123'
 // });
+
+type User = {
+  name: string;
+  surname: string;
+  email: string;
+  password: string;
+};
+
+function createOrUpdateUser<T extends Partial<User>>(initialValues: T) {
+  // Оновлення користувача
+  console.log(initialValues);
+}
+
+createOrUpdateUser({
+  email: "user@mail.com",
+  password: "password123",
+});
