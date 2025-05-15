2324b764ae6839f62b4f33d436618f43a5b13c56
PushEax <pusheax.vk@gmail.com>
2025-05-15 14:10:13 +0300

    complete generics 5

--- src/generics/5.ts
+++ src/generics/5.ts
@@ -16,3 +16,18 @@
 //   editor: 'Editor User',
 //   guest: 'Guest User',
 // };
+
+export enum UserRole {
+  admin = "admin",
+  editor = "editor",
+  guest = "guest",
+}
+
+type RoleDescriptionType = Record<UserRole, string>;
+
+// Замініть наступний код на версію за допомогою Record
+const RoleDescription: RoleDescriptionType = {
+  [UserRole.admin]: "Admin User",
+  [UserRole.editor]: "Editor User",
+  [UserRole.guest]: "Guest User",
+};
